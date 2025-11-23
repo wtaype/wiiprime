598f87fe76b68b40061e16cb20c0d07d5553cddc
wtaype <wilder.taype16@gmail.com>
2025-11-23 06:53:34 -0500

    Actualizacion Principal v10.10.60

--- smile/footer.js
+++ smile/footer.js
@@ -8,13 +8,13 @@ export let link = 'https://wtaype.github.io/';
 export let version = 'v10';
 
 /** ACTUALIZACIÓN PRINCIPAL ONE DEV [START]  
-git add . ; git commit -m "Actualizacion Principal v10.10.39" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v10.10.60" ; git push origin main
 
 // Actualizar main luego esto, pero si es mucho, solo esto. 
 git tag v10 -m "Version v10" ; git push origin v10
+
 // En caso de emergencia, para actualizar el Tag existente.  
 git tag -d v10 ; git tag v10 -m "Version v10 actualizada" ; git push origin v10 --force
-
  ACTUALIZACION TAG[END] */ 
 
 function footer(){
