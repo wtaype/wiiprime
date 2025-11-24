5ce59350918ff67f971c9c68d03728a8fb70f65c
wtaype <wtaype@users.noreply.github.com>
2025-11-24 05:17:32 +0000

    deploy: bd498b1e095420a8ea97722abdd342d679c05865

---
+++ assets/footer-Dsjq8Q1_.js
@@ -0,0 +1,8 @@
+import{$ as t}from"./main-C8Fl1__e.js";import"./vendor-B2AETYxa.js";import"./firebase-oQ0U6Yya.js";let e="WiiPrime",s=2024,o="@wilder.taype",n="https://wtaype.github.io/",p="v10";function r(){const a=new Date;return`
+  <footer class="foo wb txc psa">
+    <span>Creado con <i class="fas fa-heart"></i> by <a class="ftx lkme" href="${n}" target="_blank">${o}</a></span>
+    <span>${s} - <span class="wty">${a.getFullYear()}</span></span>
+    <span class="abw"> | ${e} ${p} | actualizado:
+    <span class="wtu">${a.toLocaleString()}</span></span>
+  </footer>
+  `}t("body").append(r());export{e as app,o as autor,r as footer,s as lanzamiento,n as link,p as version};
