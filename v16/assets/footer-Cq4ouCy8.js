163d5aaade5f10bca9cbd92a51e0471ae76de65b
wtaype <wtaype@users.noreply.github.com>
2025-11-26 04:49:47 +0000

    deploy: e3d9d4ee62b9180a1d7f64c8a06ece025c085281

---
+++ v16/assets/footer-Cq4ouCy8.js
@@ -0,0 +1,8 @@
+import{l as s,j as o,k as n,m as t,v as e,$ as p}from"./main-6W07dcAG.js";import"./vendor-B2AETYxa.js";function r(){const a=new Date;return`
+  <footer class="foo wb txc psa">
+    <span>Creado con <i class="fas fa-heart"></i> by <a class="ftx lkme" href="${s}" target="_blank">${o}</a></span>
+    <span>${n} - <span class="wty">${a.getFullYear()}</span></span>
+    <span class="abw"> | ${t} ${e} | actualizado:
+    <span class="wtu">${a.toLocaleString()}</span></span>
+  </footer>
+  `}p("body").append(r());export{r as footer};
