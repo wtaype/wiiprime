21166d0147f24e5d5d7fd11e780faaadd87fe9c8
wtaype <wilder.taype16@gmail.com>
2025-11-26 23:17:10 -0500

    Actualizacion Principal v20.10.10

--- smile/wii.js
+++ smile/wii.js
@@ -6,16 +6,16 @@ export let app = 'WiiPrime'
 export let lanzamiento = 2024;
 export let autor = '@wilder.taype';
 export let link = 'https://wtaype.github.io/';
-export let version = 'v19';
+export let version = 'v20';
 
 /** ACTUALIZACIÓN PRINCIPAL ONE DEV [START]  (1)
-git add . ; git commit -m "Actualizacion Principal v19.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v20.10.10" ; git push origin main
 
 // Actualizar main luego esto, pero si es mucho, solo esto. (2)
-git tag v19 -m "Version v19" ; git push origin v19
+git tag v20 -m "Version v20" ; git push origin v20
 
 // En caso de emergencia, para actualizar el Tag existente. (3)
-git tag -d v19 ; git tag v19 -m "Version v19 actualizada" ; git push origin v19 --force
+git tag -d v20 ; git tag v20 -m "Version v20 actualizada" ; git push origin v20 --force
  ACTUALIZACION TAG[END] */ 
 
 // ===  ⚡ CARGA INTELIGENTE v14 ===
