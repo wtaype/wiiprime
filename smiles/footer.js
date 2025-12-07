fb050098f396d585387b5756951161f3a8ef3909
wtaype <wilder.taype16@gmail.com>
2025-12-07 12:32:36 -0500

    Actualizacion Principal v28.10.12

--- smiles/footer.js
+++ smiles/footer.js
@@ -1,18 +1,15 @@
 import $ from 'jquery';
-import { wiSmart } from './widev.js';
 import {app, lanzamiento, autor, link, version} from './wii.js';
 
 export { footer };
-
 function footer(){
   const ahora = new Date();
   return `
   <footer class="foo wb txc psa">
     <span>Creado con <i class="fas fa-heart"></i> by <a class="ftx lkme" href="${link}" target="_blank">${autor}</a></span>
-    <span>${lanzamiento} - <span class="wty">${ahora.getFullYear()}</span>
-    <span>| <a href="/terminos.html" target="_blank" rel="noopener noreferrer">Privicidad y Términos</a></span>
-    <span class="abw"> | ${app} ${version} | actualizado:</span>
-    <span class="wtu">${ahora.toLocaleString()}</span>
+    <span>${lanzamiento} - <span class="wty">${ahora.getFullYear()}</span></span>
+    <span class="abw"> | ${app} ${version} | actualizado:
+    <span class="wtu">${ahora.toLocaleString()}</span></span>
   </footer>
   `;
 }; $('body').append(footer());  //Actualizar 
@@ -20,12 +17,3 @@ function footer(){
 const mstyles = `
 :root{--bgim:url("https://d35aaqx5ub95lt.cloudfront.net/images/star-pattern.svg")}.wicontainer{background: var(--bgim),linear-gradient(to bottom,var(--bg),var(--wb));}
 `;$('head').append(`<style>${mstyles}</style> `);
-
-// CARGA INTELIGENTE DE LOS CDNS 
-wiSmart({
-  css: [
-    'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap',
-    'https://fonts.googleapis.com/css2?family=Rubik:wght@300..900&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css',
-  ]
-}); 
