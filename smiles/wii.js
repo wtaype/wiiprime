f23925df4015baa673765fa3350ab7f5c026c0d1
wtaype <wilder.taype16@gmail.com>
2025-12-07 10:32:25 -0500

    Actualizacion Principal v27.10.12

--- smiles/wii.js
+++ smiles/wii.js
@@ -8,10 +8,10 @@ export let autor = '@wilder.taype';
 export let link = 'https://wtaype.github.io/';
 export let version = 'v27';
 
-/** ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (1)
+/** ACTUALIZACIÓN PRINCIPAL ONE DEV [START](1)
 git add . ; git commit -m "Actualizacion Principal v27.10.10" ; git push origin main
 
-// Actualizar main luego esto, pero si es mucho, solo esto. (2)
+// Actualizar main luego esto, pero si es mucho, solo esto.(2)
 git tag v27 -m "Version v27" ; git push origin v27
 
 // En caso de emergencia, para actualizar el Tag existente. (3)
