bb3885bfdd3990ffa8f5d83cc1cda29daed9487b
wtaype <wtaype@users.noreply.github.com>
2025-12-07 08:33:58 +0000

    deploy: 87e1c6a44f15bd33517c84658848db301645f16c

---
+++ assets/footer-CmidINKY.js
@@ -0,0 +1,11 @@
+import{$ as t,m as s}from"./main-D_U820IC.js";import"./vendor-B2AETYxa.js";let o="WiiPrime",e=2024,n="@wilder.taype",r="https://wtaype.github.io/",l="v26";function i(){const a=new Date;return`
+  <footer class="foo wb txc psa">
+    <span>Creado con <i class="fas fa-heart"></i> by <a class="ftx lkme" href="${r}" target="_blank">${n}</a></span>
+    <span>${e} - <span class="wty">${a.getFullYear()}</span>
+    <span>| <a href="/terminos.html" target="_blank" rel="noopener noreferrer">Privicidad y Términos</a></span>
+    <span class="abw"> | ${o} ${l} | actualizado:</span>
+    <span class="wtu">${a.toLocaleString()}</span>
+  </footer>
+  `}t("body").append(i());const p=`
+:root{--bgim:url("https://d35aaqx5ub95lt.cloudfront.net/images/star-pattern.svg")}.wicontainer{background: var(--bgim),linear-gradient(to bottom,var(--bg),var(--wb));}
+`;t("head").append(`<style>${p}</style> `);s({css:["https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap","https://fonts.googleapis.com/css2?family=Rubik:wght@300..900&display=swap","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"]});export{i as footer};
