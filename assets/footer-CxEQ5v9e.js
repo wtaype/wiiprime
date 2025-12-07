87c6323eb48383941c62c3a397eede11ad812184
wtaype <wtaype@users.noreply.github.com>
2025-12-07 17:32:59 +0000

    deploy: fb050098f396d585387b5756951161f3a8ef3909

---
+++ assets/footer-CxEQ5v9e.js
@@ -0,0 +1,10 @@
+import{$ as t}from"./main-BicWu3ga.js";import"./vendor-B2AETYxa.js";let e="WiiPrime",s=2024,o="@wilder.taype",n="https://wtaype.github.io/",r="v28";function l(){const a=new Date;return`
+  <footer class="foo wb txc psa">
+    <span>Creado con <i class="fas fa-heart"></i> by <a class="ftx lkme" href="${n}" target="_blank">${o}</a></span>
+    <span>${s} - <span class="wty">${a.getFullYear()}</span></span>
+    <span class="abw"> | ${e} ${r} | actualizado:
+    <span class="wtu">${a.toLocaleString()}</span></span>
+  </footer>
+  `}t("body").append(l());const p=`
+:root{--bgim:url("https://d35aaqx5ub95lt.cloudfront.net/images/star-pattern.svg")}.wicontainer{background: var(--bgim),linear-gradient(to bottom,var(--bg),var(--wb));}
+`;t("head").append(`<style>${p}</style> `);export{l as footer};
