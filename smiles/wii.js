fb825da8ab5494319c8e97f4c1652be687a1951f
wtaype <wilder.taype16@gmail.com>
2025-12-07 21:01:50 -0500

    Actualizacion Principal v34.10.10

--- smiles/wii.js
+++ smiles/wii.js
@@ -6,15 +6,15 @@ export let app = 'WiiPrime'
 export let lanzamiento = 2024;
 export let autor = '@wilder.taype';
 export let link = 'https://wtaype.github.io/';
-export let version = 'v33';
+export let version = 'v34';
 
 /** ACTUALIZACIÓN PRINCIPAL ONE DEV [START](1)
-git add . ; git commit -m "Actualizacion Principal v33.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v34.10.10" ; git push origin main
 
 // Actualizar main luego esto, pero si es mucho, solo esto.(2)
-git tag v33 -m "Version v33" ; git push origin v33
+git tag v34 -m "Version v34" ; git push origin v34
 
 // En caso de emergencia, para actualizar el Tag existente. (3)
-git tag -d v33 ; git tag v33 -m "Version v33 actualizada" ; git push origin v33 --force
+git tag -d v34 ; git tag v34 -m "Version v34 actualizada" ; git push origin v34 --force
  ACTUALIZACION TAG[END] */ 
 
