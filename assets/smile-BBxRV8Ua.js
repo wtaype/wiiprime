c65499baf0baeabf88b90b089b62debcfc93cd67
wtaype <wtaype@users.noreply.github.com>
2025-12-08 02:40:28 +0000

    deploy: e676f71f852780e99364c18e160b7f1c8eb3ac09

---
+++ assets/smile-BBxRV8Ua.js
@@ -0,0 +1,19 @@
+import{g as l,c as a,$ as t,s as o}from"./main-DBAEw3vG.js";import"./vendor-B2AETYxa.js";let s=null;const r=async()=>(s=l("smileIP")||await new Promise(e=>a(i=>(o("smileIP",i),e(i)))),console.log("🌍 IP:",s?.ip),`
+    <div class="wtm_hd">
+      <div class="hd_info">
+        <h2 class="wtm_ttl">
+          <i class="fas fa-smile-beam"></i> Bienvenidos
+        </h2>
+        <p class="wtm_sub">Disfruta de WiiPrime 😄</p>
+      </div>
+    </div>
+    <div class="wtm_lay wtm_lay--center">
+      <div class="smile_box">
+        <i class="fas fa-grin-stars smile_icon"></i>
+        <h3 class="smile_title">Tu panel de sonrisas está en construcción</h3>
+        <p class="smile_text">
+          Muy pronto verás aquí estadísticas, logros y más sorpresas.
+        </p>
+      </div>
+    </div>
+  `),m=async()=>{console.log("😊 Smile iniciado"),t(".smile_text").click(function(){alert("¡Eres genial por descubrir esto! 😄")})},d=()=>{s=null,console.log("😊 Smile limpiado")};export{d as cleanup,m as init,r as render};
